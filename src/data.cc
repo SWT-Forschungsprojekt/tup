439af691540d7cafe5e7f9c6964377583bc374c9
Max Buchholz <max.buchholz@gmx.de>
2025-02-04 15:18:13 +0100

    Fix another build error, linking does still not work

---
+++ src/data.cc
@@ -0,0 +1,20 @@
+#include "tup/data.h"
+
+#include <filesystem>
+#include <future>
+
+#include "tup/tt_location_rtree.h"
+
+namespace fs = std::filesystem;
+namespace n = nigiri;
+
+namespace tup {
+  void data::load_tt(fs::path const& p) {
+    tags_ = tag_lookup::read(path_ / "tags.bin");
+    tt_ = n::timetable::read(path_ / p);
+    tt_->locations_.resolve_timezones();
+    location_rtee_ = std::make_unique<point_rtree<n::location_idx_t>>(
+        create_location_rtree(*tt_));
+    init_rtt();
+  }
+}// namespace tup
