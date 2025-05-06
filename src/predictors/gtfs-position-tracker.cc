d751b8ec45296180e335011b165b0a5c6c0d384f
Max Buchholz <max.buchholz@gmx.de>
2025-05-06 11:39:47 +0200

    Replace auto with proper typing

--- src/predictors/gtfs-position-tracker.cc
+++ src/predictors/gtfs-position-tracker.cc
@@ -125,11 +125,11 @@ std::vector<nigiri::location> get_stops_for_trip(nigiri::timetable const& timeta
     // Nur Debug-Meldung vor dem Zugriff
     std::cerr << "Versuche Zugriff auf ersten Transport..." << std::endl;
     // Ersten Transport nehmen und dessen Route
-    const auto [first, second] = transports[0];
+    const cista::strong<unsigned, nigiri::_transport_idx> first = transports[0].first;
     std::cerr << "Erster Transport erfolgreich gelesen" << std::endl;
     std::cerr << "Größe von transport_route_: " << timetable.transport_route_.size() << std::endl;
     std::cerr << "Versuche Zugriff auf Transport-Route..." << std::endl;
-    auto const route_idx = timetable.transport_route_.at(first);
+    nigiri::route_idx_t const route_idx = timetable.transport_route_.at(first);
 
     // Stops aus der Route-Sequenz holen
     std::vector<nigiri::location> stops;
@@ -138,8 +138,8 @@ std::vector<nigiri::location> get_stops_for_trip(nigiri::timetable const& timeta
     // Jeden Stop in ein location-Objekt umwandeln
     std::cerr << "Versuche Zugriff auf Transport-Route..." << std::endl;
     std::cerr << "Size of timetable.locations_.ids_" << timetable.locations_.ids_.size() << std::endl;
-    for (auto const location_idx_t : stop_sequence) {
-      auto const stop_idx = nigiri::stop{location_idx_t}.location_idx();
+    for (unsigned const location_idx_t : stop_sequence) {
+      nigiri::location_idx_t const stop_idx = nigiri::stop{location_idx_t}.location_idx();
       stops.push_back(timetable.locations_.get(nigiri::location_idx_t(stop_idx)));
     }
     return stops;
