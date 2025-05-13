a69d0de6a8fc8218423d19c4c74be71e6c4aca86
Malte <malte.meiners@greenteam-stuttgart.de>
2025-05-13 16:48:28 +0000

    fix: Remove extra qualification

--- include/predictors/predictor-utils.h
+++ include/predictors/predictor-utils.h
@@ -7,6 +7,6 @@ class predictorUtils {
   public:
     static std::vector<nigiri::location> get_stops_for_trip(nigiri::timetable const& timetable, std::string const& trip_id);
     static auto convert_trip_id_to_idx(nigiri::timetable const& timetable, std::string const& trip_id) -> nigiri::trip_idx_t;
-    static void predictorUtils::delete_old_trip_updates(std::unordered_set<std::string> currentTripIDs, transit_realtime::FeedMessage& outputFeed);
-    static void predictorUtils::set_trip_update(std::string tripID, std::string_view stopID, std::string vehicleID, std::string routeID, int64_t newArivalTime, transit_realtime::FeedMessage& outputFeed);
+    static void delete_old_trip_updates(std::unordered_set<std::string> currentTripIDs, transit_realtime::FeedMessage& outputFeed);
+    static void set_trip_update(std::string tripID, std::string_view stopID, std::string vehicleID, std::string routeID, int64_t newArivalTime, transit_realtime::FeedMessage& outputFeed);
 };
