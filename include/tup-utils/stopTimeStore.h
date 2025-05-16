2ddc0ccfa539a9f24c5e186792949e5b6f887c42
Max Buchholz <max.buchholz@gmx.de>
2025-05-16 17:03:16 +0200

    Switch stopTimeStore implementation to standard hash map

--- include/tup-utils/stopTimeStore.h
+++ include/tup-utils/stopTimeStore.h
@@ -1,45 +1,20 @@
 #pragma once
 
-#include <cista/containers/mmap_vec.h>
-#include <cista/containers/paged.h>
-#include <cista/containers/paged_vecvec.h>
-#include <cista/mmap.h>
-#include <cista/strong.h>
+#include <cstdint>
 
-#include <filesystem>
-#include <optional>
 #include <string>
-
-using stop_time_idx_t = cista::strong<std::uint32_t, struct stoptime_idx>;
+#include <unordered_map>
 
 /**
  * An Event when a vehicle of a trip arrives or ends at a stop
  */
 struct stopTime {
-  stop_time_idx_t idx;
   std::string trip_id;
   std::string stop_id;
-  std::optional<int64_t> arrival_time;
-  std::optional<int64_t> departure_time;
+  std::int64_t arrival_time;
   std::string date;
 };
 
-template <typename T>
-using mm_vec = cista::basic_mmap_vec<T, std::uint64_t>;
-
-template <typename Key, typename T>
-/**
- * Helper to deal with the memory-mapped paged double vector
- */
-struct mm_paged_vecvec_helper {
-  using data_t =
-      cista::paged<mm_vec<T>, std::uint64_t, std::uint32_t, 2U, 1U << 31U>;
-  using idx_t = mm_vec<typename data_t::page_t>;
-  using type = cista::paged_vecvec<idx_t, data_t, Key>;
-};
-
-template <typename Key, typename T>
-using mm_paged_vecvec = typename mm_paged_vecvec_helper<Key, T>::type;
 
 /**
  * Stores historic stopTimes which are then used for prediction
@@ -48,18 +23,13 @@ class stopTimeStore {
   /**
    * Create a stopTimeStore storing the memory-mapped files
    */
-  mm_paged_vecvec<stop_time_idx_t, stopTime> data_;
 public:
-  stopTimeStore(std::filesystem::path, cista::mmap::protection);
-  cista::mmap mm(char const* file);
+  std::unordered_map<std::tuple<std::string, std::string, std::string>,
+                     std::int64_t> stopTimes;
 
-  cista::mmap::protection mode_;
-  std::filesystem::path p_;
-
-  static void store(std::string const& trip_id,
+  void store(std::string const& trip_id,
                     std::string const& stop_id,
-                    std::optional<int64_t> arrival_time,
-                    std::optional<int64_t> departure_time,
-                    stopTimeStore& storage);
-  static int64_t getAverageArrivalTime(std::string const& trip_id, std::string const& stop_id, stopTimeStore& storage);
+                    std::int64_t arrival_time,
+                    std::string date);
+  int64_t getAverageArrivalTime(std::string const& trip_id, std::string const& stop_id);
 };
--- src/tup-utils/stopTimeStore.cc
+++ src/tup-utils/stopTimeStore.cc
@@ -1,46 +1,20 @@
 #include "tup-utils/stopTimeStore.h"
 
-namespace fs = std::filesystem;
-
-stopTimeStore::stopTimeStore(std::filesystem::path path,
-                               cista::mmap::protection const mode)
-    : mode_{mode},
-      p_{[&]() {
-        fs::create_directories(path);
-        return std::move(path);
-      }()},
-      data_{mm_paged_vecvec_helper<stoptime_idx_t, stopTime>::data_t{
-        mm_vec<stopTime>{mm("stoptimes_data.bin")}},
-    mm_vec<cista::page<std::uint64_t, std::uint32_t>>{
-      mm("stoptimes_idx.bin")}} {}
-
-cista::mmap stopTimeStore::mm(char const* file) {
-  return cista::mmap{(p_ / file).generic_string().c_str(), mode_};
-}
-
 void stopTimeStore::store(std::string const& trip_id,
                           std::string const& stop_id,
-                          std::optional<int64_t> arrival_time,
-                          std::optional<int64_t> departure_time,
-                          stopTimeStore& storage) {
-    stopTime new_stop_time{
-        .trip_id = trip_id,
-        .stop_id = stop_id,
-        .arrival_time = arrival_time,
-        .departure_time = departure_time
-    };
-    storage.data_.emplace_back({new_stop_time});
+                          std::int64_t arrival_time,
+                          std::string date) {
+  const auto key = std::make_tuple(trip_id, stop_id, date);
+  stopTimes[key] = arrival_time;
 }
 
-int64_t stopTimeStore::getAverageArrivalTime(std::string const& trip_id, std::string const& stop_id, stopTimeStore& storage) {
+int64_t stopTimeStore::getAverageArrivalTime(std::string const& trip_id, std::string const& stop_id) {
   int64_t sum = 0;
   int64_t count = 0;
-  for (auto const& stoptime : storage.data_) {
-    if (stoptime.front().trip_id == trip_id && stoptime.front().stop_id == stop_id) {
-      if (stoptime.front().arrival_time.has_value()) {
-        sum += stoptime.front().arrival_time.value();
-        count++;
-      }
+  for (auto const& stoptime : stopTimes) {
+    if (std::get<0>(stoptime.first) == trip_id && std::get<1>(stoptime.first) == stop_id) {
+      sum += stoptime.second;
+      count++;
     }
   }
   return sum / count;
